8425465110d153d656e56bef1a697a8f2df37453
9ShAdow <95073328+9ShAdow@users.noreply.github.com>
2022-12-19 05:51:47 +0100

    J-?
    J-...

--- client.py
+++ client.py
@@ -1,7 +1,12 @@
 import socket
-
+import  os 
+import psutil
+import platform
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import * 
+import sys
 #on va faire un ficher pour lancer tt les serveurs en meme temps ensuite en fonction des num on pourra choisir le serveur
-
+"""
 client_socket = socket.socket()
 print("Socket créé.")
 host = "localhost"
@@ -19,11 +24,61 @@ print("Connecté au serveur.")
 message =input("envoi d'un message : ")
 client_socket.send(message.encode())
 print("Message envoyé")
+"""
 
 
+
+
+class client(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Client")
+        grid = QGridLayout()
+        widget = QWidget()
+        self.setCentralWidget(widget) 
+        widget.setLayout(grid)
+        self.label = QTextEdit()
+
+        ram= QPushButton("ram")
+        cpu=QPushButton("cpu")
+
+        grid.addWidget(ram, 0, 0)
+        grid.addWidget(cpu , 1, 0)
+
+        ram.clicked.connect(self.__actionram)
+        cpu.clicked.connect(self.__actioncpu)
+
+
+    def __actionram(self):
+        message = "ram"
+        client_socket.send(message.encode())
+        print("La requête pour la ram a été envoyée")
+        data = client_socket.recv(1024).decode()
+        
+        print(f"Message du serveur : {data}")
+        self.label.append(f"{data}\n")
+        self.label.append(f"")
+        
+        
+   
+
+    def __actioncpu(self):
+        message = "cpu"
+        client_socket.send(message.encode())
+        print("La requête pour la cpu a été envoyée")
+        data = client_socket.recv(1024).decode()
+        print(f"Message du serveur : {data}")
+        self.label.append(f"{data}\n")
+
+"""
 if message == "arret" or message == "bye":
     print("Connexion terminé.")
     client_socket.close()
+
+elif message == "ram":
+    __actionram(self)
+elif message == "cpu":
+    __actioncpu(self)   
 else:
     data = client_socket.recv(1024).decode()
     '''print("Message reçu du serveur:")
@@ -31,6 +86,7 @@ else:
     if data == "arret":
         print("Connexion terminé.")
         client_socket.close()
+
     else:
         while data !="bye":
             message = input("envoi d'un message : ")
@@ -47,3 +103,25 @@ else:
                 print("Connexion terminé.")
                 client_socket.close()
                 break
+"""
+
+if __name__=="__main__":
+    client_socket = socket.socket()
+    print("Socket créé.")
+    host = "localhost"
+    port = input("Choisissez le serveur de connexion : ")
+
+    if port == "1":
+        port = 1111
+    elif port == "2":
+        port = 10000
+    else:
+        print("Serveur introuvable !")
+
+    client_socket.connect((host, port))
+    print("Connecté au serveur.")
+
+    app = QApplication(sys.argv)
+    window = client()
+    window.show()
+    app.exec()
--- server1.py
+++ server1.py
@@ -2,6 +2,8 @@ import socket
 import  os 
 import psutil
 import platform
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
 
 
 #host devra changer avec un ficher CSV 
@@ -15,6 +17,7 @@ print("Socket sur l'adresse {} et le port {}".format(host, port))
 server_socket.listen(1)
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
+str_2 = psutil.cpu_percent()
 
 print("En attente du client")
 conn, address = server_socket.accept()
@@ -28,24 +31,54 @@ mem_usage = psutil.virtual_memory()
 
 
 data = conn.recv(1024).decode()
+data = data.lower()
 
-
+# DIFFERENTES FONCTIONS 
 
 def ram():
-  
-    print(f"RAM Totale: {mem_usage.total/(1024**3):.2f}G")
-    print(f"RAM Libre: {mem_usage.percent}%")
-    print(f"RAM Utilisé: {mem_usage.used/(1024**3):.2f}G")
+    str_1 = mem_usage.percent
+    str_2 = mem_usage.total/(1024**3)
+    
+    #str_2 = psutil.cpu_percent(4)
+    #conn = str("CPU % used:", psutil.cpu_percent())
+    #server_socket.send(rep.encode())
+    
+    #print('The CPU usage is: ', psutil.cpu_percent(4), '%')
+    rep = str(f"La RAM Libre est: {str_1} % \nLa RAM Utilisé est: {str_2}")
+    conn.send(rep.encode())
+    
+ 
+    
+    
+    #print(f"RAM Totale: {mem_usage.total/(1024**3):.2f}G")
+    #print(f"RAM Libre: {mem_usage.percent}%")
+    #print(f"RAM Utilisé: {mem_usage.used/(1024**3):.2f}G")
     #print('RAM memory % used:', psutil.virtual_memory()[2])
     # Getting usage of virtual_memory in GB ( 4th field)
     #print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
 def cpu():
-    print('CPU % used:', psutil.cpu_percent())
-    print('The CPU usage is: ', psutil.cpu_percent(4), '%')
+    str_1 = psutil.cpu_percent()
+    str_2 = psutil.cpu_percent(4)
+    #conn = str("CPU % used:", psutil.cpu_percent())
+    #server_socket.send(rep.encode())
+    
+    #print('The CPU usage is: ', psutil.cpu_percent(4), '%')
+    rep = str(f" The CPU used is: {str_1} \n The CPU usage is: {str_2} %")
+    conn.send(rep.encode())
+    #
+    # 
+    # print(rep)
+    
+
+
+    #server_socket.send(rep.encode())
 def osn():
     print('OS:', platform.system(), "version", platform.release())
+    #server_socket.send(data.encode())
 def ip():
+    #rep= str("L'addresse IP de la machine est :", IPAddr)
     print("L'addresse IP de la machine est :", IPAddr)
+    #server_socket.send(rep.encode())
 def name():
     print('Le nom de la machine est :', platform.node())
     '''on peut aussi faire hostname = socket.gethostname()
@@ -60,16 +93,33 @@ def dir():
 
 
 while data !="arret":
-    data = data.lower()
+    #conn.recv(1024).decode()
+    #data = conn.recv(1024).decode()
+    #data = data.lower()
+    #reply = ("saisir un message: ")
+    #conn.send(reply.encode())
+
     reply = ("saisir un message: ")
     conn.send(reply.encode())
+    print("Réponse envoyé")
+    data = conn.recv(1024).decode()
+    print(f"Message {data} reçue du client:")
+
+
+    data = data.lower()
+    
+
     if data == "arret":
         print("Connexion terminé")
         conn.close()
     elif data == "ram":
+        print("RAM")
         ram()
+        pass
     elif data == "cpu":
+        print("CPU")
         cpu()
+        pass
     elif data == "os":
         osn()
     elif data == "ip":
@@ -78,28 +128,31 @@ while data !="arret":
         name()
     elif data =="dos:dir":
         dir()
-    #elif data =="dos:mkdir":
+    elif data.startswith("dos:") and platform.system() == "Windows":
         #split apres : 
         #partie apres : va dans vartiable 
-        #cmd = data.split(":")
-        #os.system(cmd2)
+        cmd = data.split(":")
+        os.system(cmd[1])
+        conn.send.data(cmd)
+
+    elif data.startswith("Linux:") and platform.system() == "Linux":
+        cmd = data.split(":")
+        os.system(cmd[1])
+        print("ok")
     else:
         print("La commande n'existe pas cliquer sur l'aide ou taper --help pour pour voir l'ensemble des commandes possibles")
         #faire un truc qui change la couleur du texte de aide dans la IG du client
         pass
-    print("Réponse envoyé")
-    data = conn.recv(1024).decode()
-    print(f"Message {data} reçue du client:")
+
+    reply = ("le message a bien été reçu")
+    conn.send(reply.encode())
+
+print(f"Message {data} reçue du client:")
+print("Réponse envoyé")
+    #data = conn.recv(1024).decode()
     
-  
-
-print("Message", data,"received from the client:")
-
-
- 
-
-
-
+    
+   
 
 print("Connexion terminé.")
 conn.close()
