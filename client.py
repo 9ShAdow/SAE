ac4c61d982ae7ad635f96239d5c85933540db4c6
9ShAdow <95073328+9ShAdow@users.noreply.github.com>
2022-12-19 07:31:07 +0100

    J-?
    ?

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import platform
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import * 
 import sys
+from requests import Session
 #on va faire un ficher pour lancer tt les serveurs en meme temps ensuite en fonction des num on pourra choisir le serveur
 """
 client_socket = socket.socket()
@@ -38,15 +39,64 @@ class client(QMainWindow):
         self.setCentralWidget(widget) 
         widget.setLayout(grid)
         self.label = QTextEdit()
+        
+
+        name = 'shad' # Enter your name here!
+        chat_url = 'https://build-system.fman.io/chat'
+        server = Session()
 
         ram= QPushButton("ram")
         cpu=QPushButton("cpu")
+        name=QPushButton("name")
+        
+        #ok#
+        text_area = QTextEdit()
+        text_area.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        message = QLineEdit()
+        layout = QVBoxLayout()
+        layout.addWidget(text_area)
+        layout.addWidget(message)
+
+        #evenement
+
+        # Event handlers:
+        def display_new_messages():
+            new_message = server.get(chat_url).text
+            if new_message:
+                text_area.append(new_message)
+
+        def send_message():
+            server.post(chat_url, {'name': name, 'message': message.text()})
+            message.clear()
+
+        # Signals:
+        message.returnPressed.connect(send_message)
+        timer = QTimer()
+        timer.timeout.connect(display_new_messages)
+        timer.start(1000)
+
+        
+
+        
+
 
         grid.addWidget(ram, 0, 0)
         grid.addWidget(cpu , 1, 0)
+        grid.addWidget(name, 2, 0)
+        grid.addWidget(text_area, 0, 1, 3, 1)
+        grid.addWidget(message, 3, 0, 1, 2)
+        
+    
 
         ram.clicked.connect(self.__actionram)
         cpu.clicked.connect(self.__actioncpu)
+        name.clicked.connect(self.__actionname)
+        
+
+        #on essaye
+
+        
+        #on essaye 
 
 
     def __actionram(self):
@@ -66,6 +116,14 @@ class client(QMainWindow):
         message = "cpu"
         client_socket.send(message.encode())
         print("La requête pour la cpu a été envoyée")
+        data = client_socket.recv(1024).decode()
+        print(f"Message du serveur : {data}")
+        self.label.append(f"{data}\n")
+
+    def __actionname(self):
+        message = "name"
+        client_socket.send(message.encode())
+        print("La requête pour le nom a été envoyée")
         data = client_socket.recv(1024).decode()
         print(f"Message du serveur : {data}")
         self.label.append(f"{data}\n")
